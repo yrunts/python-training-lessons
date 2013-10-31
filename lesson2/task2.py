78a0d33232c04fb560aa78fddbf9bc9255d1c5c1
yrunts <yevhenrunts@gmail.com>
2013-10-31 16:30:19 +0200

    add #2 task 2

---
+++ lesson2/task2.py
@@ -0,0 +1,31 @@
+"""Solution to task 2 from lesson 2."""
+
+class MyNumberPrinter(object):
+    """Number printer. Performs print operation with numbers.
+
+    :IVariables:
+        - `n`: an `int` number;
+    """
+    def __init__(self, n):
+        super(MyNumberPrinter, self).__init__()
+        self.n = n
+
+    def me(self):
+        pass
+
+    def factorial(self):
+        pass
+
+    def string(self):
+        pass
+
+    def update(self):
+        pass
+
+    def time_in_past(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
+
