aafe041a638c0f8046050801ad796c9830c9c4ea
yrunts <yevhenrunts@gmail.com>
2013-11-01 15:14:00 +0200

    solve task2 from lesson2

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -1,30 +1,73 @@
 """Solution to task 2 from lesson 2."""
 
+
+import math
+import datetime
+
 class MyNumberPrinter(object):
     """Number printer. Performs print operation with numbers.
 
     :IVariables:
         - `n`: an `int` number;
+        - `time_formats`: accepeted time formats values;
     """
+    time_formats = ('s', 'm', 'h', 'd')
+
     def __init__(self, n):
         super(MyNumberPrinter, self).__init__()
         self.n = n
 
     def me(self):
-        pass
+        """Prints current number."""
+        print 'Number is %s' % str(self.n)
 
     def factorial(self):
-        pass
+        """Prints current number factorial."""
+        print 'Factorial for %s is equal to %s' % (self.n,
+                                                   math.factorial(self.n))
 
-    def string(self):
-        pass
+    def string(self, s):
+        """Prints a string concatenated with itself current number times.
 
-    def update(self):
-        pass
+        :Parameters:
+            - `s`: an `string`;
+        """
+        print s * self.n
 
-    def time_in_past(self):
-        pass
+    def update(self, n):
+        self.n = n
+        self.me()
 
+    def time_in_past(self, format):
+        """Prints past time from now based on delta format.
+
+        :Parameters:
+            - `format`: an `string` time delta format;
+        """
+        if format in self.time_formats:
+            delta = self.n
+            if format == 'm':
+                delta *= 60
+            elif format == 'h':
+                delta *= 60 * 60
+            elif format == 'd':
+                 delta *= 60 * 60 * 24
+            past = datetime.datetime.now() - datetime.timedelta(seconds=delta)
+            print 'time is %s' % past
+
+
+def main():
+    a = MyNumberPrinter(10)
+    a.me()
+    a.factorial()
+    a.string('a1')
+    a.update(5)
+    a.factorial()
+    a.string('a1')
+    a.time_in_past('s')
+    a.time_in_past('m')
+    a.time_in_past('h')
+    a.time_in_past('d')
 
 if __name__ == '__main__':
     main()
