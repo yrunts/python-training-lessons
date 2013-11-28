3014111fa1218862072a35567fdf0447d3b95ce9
yrunts <yevhenrunts@gmail.com>
2013-11-28 18:04:54 +0200

    solution for task8

---
+++ lesson8/task.py
@@ -0,0 +1,115 @@
+"""Solution to tasks from lesson 8."""
+
+
+import types
+import contextlib
+from functools import wraps, partial
+
+# global transactions counter
+t_count = 0
+
+def transaction_manager(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        global t_count
+        t_count += 1
+        print 'Transaction %s for %s started' % (t_count, func.__name__)
+        try:
+            func(*args, **kwargs)
+        except Exception, e:
+            print 'Transaction %s for %s cancelled' % (t_count, func.__name__)
+        else:
+            print 'Transaction %s for %s completed' % (t_count, func.__name__)
+    return wrapper
+
+
+@transaction_manager
+def example1():
+    print 'example 1'
+
+
+@transaction_manager
+def example2():
+    raise Exception()
+
+
+@transaction_manager
+def example3(text):
+    print text
+
+
+def print_decorator(text):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
+            print text
+        return wrapper
+    return decorator
+
+
+@print_decorator('World')
+def example4(arg):
+    print arg
+
+
+@contextlib.contextmanager
+def transaction(status):
+    global t_count
+    t_count += 1
+    count = t_count
+    print 'Transaction %s started' % count
+    try:
+        yield
+    except Exception, e:
+        print 'Transaction %s cancelled' % count
+    else:
+        print 'Transaction %s completed' % count
+
+
+def my_func(a, b, c):
+    with transaction('root'):
+        print a
+        with transaction('nested successful'):
+            print b
+        with transaction('nested with error'):
+            print c
+            raise Exception
+
+
+def template_methods(template, method_table):
+    def decorator(cls):
+        for k, v in method_table.iteritems():
+            method = partial(template, **v)
+            setattr(cls, k, types.MethodType(method, cls))
+        return cls
+    return decorator
+
+
+def template(self, a, b, c):
+    print self.x, a, b, c
+
+method_table = {
+    'test': dict(a=10, c=20),
+    'other_test': dict(b=30),
+}
+
+@template_methods(template, method_table)
+class A(object):
+    x = 10
+
+
+def main():
+    example1()
+    example2()
+    example3('Hi')
+    example4('Hello')
+
+    my_func(1, 2, 3)
+
+    a = A()
+    a.test(b=2)
+    a.other_test(a=2, c=2)
+
+
+if __name__ == '__main__':
+    main()
